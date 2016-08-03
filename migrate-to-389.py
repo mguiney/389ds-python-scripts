480c0b047bf37ee09fb5164bcc2806a654d67daa
test389 Root <root@test389.cat.pdx.edu>
2016-08-03 16:47:48 -0700

    added file cleanup function

--- migrate-to-389.py
+++ migrate-to-389.py
@@ -1,6 +1,21 @@
 #!/usr/bin/python3
 
 from ldap3 import *
+import os
+
+def ldif_cleanup(filename):
+  newfile = filename[1:] 
+  nfo = open(newfile, "a")
+  with open(filename) as oldfile:  
+    for line in oldfile: 
+      if '#' in line: 
+        line = "\n"
+      elif 'version:' in line: 
+        line = "\n" 
+      nfo.write(line)
+  nfo.close()
+  return newfile
+
 
 def branchscrape(x, filename):
   i = 0
@@ -12,7 +27,9 @@ def branchscrape(x, filename):
   while i < len(c.entries):
     fo.write(str(c.entries[i].entry_to_ldif()))
     i = i + 1
-
+    newfile = ldif_cleanup(filename)
+    os.remove(filename)
+    return newfile
 
 def get_filename(x):
   x = x[3:]
@@ -42,6 +59,6 @@ def main():
   branches = get_branches()
   for x in branches:
     filename = get_filename(x)
-    branchscrape(x, filename)
+    newfile = branchscrape(x, filename)
 
 main()
